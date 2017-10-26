7578329a6d5c54ad4e3b4f08863b440d89030dac
Greg Friedlander <gregfriedlander@gmail.com>
2017-10-25 17:43:41 -0700

    admin_app nearing completion

--- apps/login_app/models.py
+++ apps/login_app/models.py
@@ -77,6 +77,7 @@ class Artist(models.Model):
     record=models.ForeignKey(Record, related_name='artists')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
     
 
     
