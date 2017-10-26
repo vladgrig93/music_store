77d8ed46621afeba2f43b7dc5e2f14ccadf0f628
Michael Topmiller <michaeltopmiller@gmail.com>
2017-10-26 15:26:32 -0700

    css_updates

--- apps/users_app/urls.py
+++ apps/users_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns=[
     url(r'^viewcart$', views.viewcart),
     url(r'^category$', views.category),
     url(r'^artist/(?P<artist_id>\d+)$', views.artist),
-    url(r'^record/(?P<artist_record_id>\d+)$', views.record),]
+    url(r'^record/(?P<artist_record_id>\d+)$', views.record),
     url(r'^settings$', views.settings),
     
 ]
