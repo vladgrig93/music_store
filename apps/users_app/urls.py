7e4e2acde867ecf27c280950c0329e0159a177ac
Vlad <vpgrigoryan@gmail.com>
2017-10-26 11:23:51 -0700

    updating with record, artist functinality

--- apps/users_app/urls.py
+++ apps/users_app/urls.py
@@ -4,4 +4,5 @@ urlpatterns=[
     url(r'^user_portal$', views.index),
     url(r'^viewcart$', views.viewcart),
     url(r'^category$', views.category),
-    url(r'^artist$', views.artist),]
+    url(r'^artist/(?P<artist_id>\d+)$', views.artist),
+    url(r'^record/(?P<artist_record_id>\d+)$', views.record),]
